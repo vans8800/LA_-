20cb83739f3e69c9b0297a80597d69ad451ace39
vans8800 <muwang86@126.com>
2024-09-22 11:17:15 +0800

    Update get_current_PC-value.c

--- get_current_PC-value.c
+++ get_current_PC-value.c
@@ -4,7 +4,8 @@
 static long* get_PC(void){
 
     unsigned long *val;
-
+    //把当前函数的返回地址，写入val
+    //当前函数的返回地址=调用该函数时的PC
     asm volatile ("move %0, $r1":"=r"(val));
     return val;
 }
@@ -12,7 +13,12 @@ static long* get_PC(void){
 
 int main()
 {
-    printf("PC val is: %ld\n",get_PC());
-    printf("PC val is: %lu\n",get_PC());
+    long *ret = get_PC();
+    //下面两种打印方式等价
+    printf("PC val is: %lu\n", ret);
+    printf("PC val is: %p\n", ret);
+
+
+    //printf("PC val is: %lu\n", *ret);
     return 0;
 }
